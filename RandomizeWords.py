8c0c34e23eccad223746958aa7e4f1696c23d72d
Sai Pc <schand31@asu.edu>
2015-09-10 16:55:45 -0700

    Randomizer complete

--- RandomizeWords.py
+++ RandomizeWords.py
@@ -1,1 +1,16 @@
+from random import shuffle
 __author__ = 'saipc'
+
+def RandomizeWords(string):
+    newStringList = []
+    for word in string.split(" "):
+        # print word
+        newStringList.append(word)
+    shuffle(newStringList)
+    # print(newStringList)
+    newString = ""
+    newString = " ".join(newStringList)
+    print newString
+
+
+RandomizeWords("hello im sai and my name is also sai and this dummy text is also sai")
