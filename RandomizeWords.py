2b90b91f59c204445d8fb0daf07afee9c78d41eb
Sai Pc <schand31@asu.edu>
2015-09-11 23:58:17 -0700

    random push tried to randomize letters too

--- RandomizeWords.py
+++ RandomizeWords.py
@@ -8,9 +8,13 @@ def RandomizeWords(string):
         newStringList.append(word)
     shuffle(newStringList)
     # print(newStringList)
+    # shuffledWordList =["".join(shuffle[word]) for word in newStringList]
+
     newString = ""
     newString = " ".join(newStringList)
     print newString
+    # print shuffledWordList
 
 
 RandomizeWords("hello im sai and my name is also sai and this dummy text is also sai")
+RandomizeWords("Hello this is a random string for a push")
