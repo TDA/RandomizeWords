81edbedbfa8b1a70a715e85b1d7f0b32d8f644f1
Sai Pc <schand31@asu.edu>
2015-09-12 11:16:52 -0700

    letter reaarange

--- RandomizeWords.py
+++ RandomizeWords.py
@@ -3,18 +3,27 @@ __author__ = 'saipc'
 
 def RandomizeWords(string):
     newStringList = []
-    for word in string.split(" "):
+    for word in string.split():
         # print word
         newStringList.append(word)
     shuffle(newStringList)
     # print(newStringList)
-    # shuffledWordList =["".join(shuffle[word]) for word in newStringList]
-
+    shuffledWordList = [[letter for letter in word] for word in newStringList]
     newString = ""
     newString = " ".join(newStringList)
     print newString
-    # print shuffledWordList
+    print shuffledWordList
 
 
-RandomizeWords("hello im sai and my name is also sai and this dummy text is also sai")
+RandomizeWords("hello, im sai and my name is also sai and this dummy text is also sai")
 RandomizeWords("Hello this is a random string for a push")
+
+
+x = [0] * 26
+linearr = []
+
+pos = ord('a') - 97
+value = linearr[1]
+
+x.insert(pos, 1)
+print x
